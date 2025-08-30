3c2cfae856dd5b35e070693073608681d788a861
Michael McLaren <m.mclaren42@gmail.com>
2025-08-29 18:04:46 -0700

    Remove option setting from .onLoad()
    The package supported two redundant approaches to default Twist options
    for token and workspace directory (but not workspace id), which was
    leading to confusing behavior when using the package. This commit
    removes the setting of options() from environment variables that took
    place in .onLoad(). Setting will now happen in the `twist_*` functions
    defined in api.R.

--- rpkg/R/zzz.R
+++ rpkg/R/zzz.R
@@ -1,12 +1,2 @@
 .onLoad <- function(libname, pkgname) {
-  # Check for environment variables on package load
-  token <- Sys.getenv("TWIST_TOKEN")
-  if (token != "") {
-    options(twist_token = token)
-  }
-
-  dir <- Sys.getenv("TWIST_WORKSPACE_DIR")
-  if (dir != "") {
-    options(twist_workspace_dir = dir)
-  }
 }
